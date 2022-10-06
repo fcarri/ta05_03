4bf0415591825c413ab0e54c49022003e6c33b41
fcarri <fcs0000@gmail.com>
2022-10-06 19:15:43 +0200

    Commit 11

---
+++ ta05_3.java
@@ -0,0 +1,10 @@
+package project5;
+import javax.swing.JOptionPane;
+public class ta05_3 {
+public static void main(String args[]){
+		
+		String nom = JOptionPane.showInputDialog("Introdueix el teu nom: ");
+		JOptionPane.showMessageDialog(null, "El meu nom és "+nom);
+	}
+
+}
